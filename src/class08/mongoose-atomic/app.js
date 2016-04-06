f9488e8f8f5ddb993a8837e2dbfc6248a74a7884
Filipe Leuch Bonfim <filipe1309@gmail.com>
2016-04-06 00:10:56 -0300

    nodejs: Add class 08 part 1 source files
    Signed-off-by: Filipe Leuch Bonfim <filipe1309@gmail.com>

---
+++ src/class08/mongoose-atomic/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('./db/config');
+const CRUD = require('./pokemon-controller');
+const query = { name: /atomicmon/i };
+const data = {
+    name: 'Atomicmon',
+    description: 'Vai que vai',
+    type: 'terra',
+    attack: 69,
+    defense: 96,
+    height: 0.7,
+  };
+const mod = { attack: 51 };
+
+//CRUD.create(data);
+//CRUD.retrieve(query);
+//CRUD.update(query, mod, { multi: true });
+CRUD.delete(query);
